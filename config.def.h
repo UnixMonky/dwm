cdd2f13c1c127560c811bbe8cec27123ba41f30c
Matthew Huber <unixmonky@gmail.com>
2019-01-22 15:07:53 -0500

    Define startup windows/tags

--- config.def.h
+++ config.def.h
@@ -30,9 +30,13 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class         instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",        NULL,       NULL,       0,            1,           -1 },
+	{ "Firefox",     NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Mailspring",  NULL,       NULL,       1 << 2,       0,            0 },
+	{ "Slack",       NULL,       NULL,       1 << 7,       1,            0 },
+	{ "discord",     NULL,       NULL,       1 << 7,       1,            0 },
+	{ "Skype",       NULL,       NULL,       1 << 7,       1,            0 },
 };
 
 /* layout(s) */
