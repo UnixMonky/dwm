17b7f2478ab904765b1ea8cbd533f305fd49c448
Matthew Huber <unixmonky@gmail.com>
2019-01-24 13:53:26 -0500

    font was a little too big

--- config.def.h
+++ config.def.h
@@ -11,8 +11,8 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 // static const char *fonts[]          = { "monospace:size=10" };
 static const char *fonts[]          = {
-	"DejaVu Sans Mono:pixelsize=20",
-	"Material Design Icons:pixelsize=20",
+	"DejaVu Sans Mono:pixelsize=14",
+	"Material Design Icons:pixelsize=14",
 };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
